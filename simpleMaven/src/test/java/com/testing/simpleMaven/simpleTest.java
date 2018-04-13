9f3ef405948e84ad5893aabf5535f41fc5074a98
SindhujaChinnathambi <sindhu13.c@gmail.com>
2018-04-12 22:39:45 -0700

    new commit

---
+++ simpleMaven/src/test/java/com/testing/simpleMaven/simpleTest.java
@@ -0,0 +1,10 @@
+package com.testing.simpleMaven;
+
+public class simpleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
